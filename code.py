e1fbe8b28290257abb385172d19ec17a438684a1
Snaatan Raina <snaatanraina@gmail.com>
2026-09-09 21:52:15 +0530

    Update code.py

--- code.py
+++ code.py
@@ -5,4 +5,11 @@ def greet(name):
     return f"Hello, {name}!"
 
 print(greet("Matthias"))
-ss
+
+#guys i just learned about Regex in Python!
+
+import re
+
+text = "Meetings are on 2026-09-10, 2025-12-31, and 26-01-01. Ignore 2026-9-5 and 2026-123-45."
+
+answer = re.findall(r"\b\d{4}-\d{2}-\d{2}\b", text)
