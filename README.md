e4661a50d57174ffa4f24ba9e9d19943229ad848
Snaatan Raina <snaatanraina@gmail.com>
2026-09-10 12:18:17 +0530

    just some more of my experiments with the things im learning

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Setebaid-AI
+
 reverse diabetes..... literally
+
+rn Im just learning python, and solving questions on coursera, hackerrank and experimenting with the things I learn
--- code.py
+++ code.py
@@ -13,3 +13,13 @@ import re
 text = "Meetings are on 2026-09-10, 2025-12-31, and 26-01-01. Ignore 2026-9-5 and 2026-123-45."
 
 answer = re.findall(r"\b\d{4}-\d{2}-\d{2}\b", text)
+
+#sortin a tuple without using sorted() function
+
+my_tuple = (5, 2, 9, 1, 7)
+
+# Convert to list, sort, then convert back to tuple
+
+my_list = [x for x in my_tuple]
+my_list.sort()
+sorted_tuple = tuple(my_list)
